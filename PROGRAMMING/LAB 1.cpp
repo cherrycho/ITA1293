28573798182fd6c3f8ac94ea5d28710c3908a5d3
Cherry <60821798+cherrycho@users.noreply.github.com>
2022-12-15 11:43:06 +0800

    Update LAB 1.cpp

--- PROGRAMMING/LAB 1.cpp
+++ PROGRAMMING/LAB 1.cpp
@@ -1,3 +1,29 @@
+/* 
+l. Write C++ statements that produce the following output:
+
+    Name: //output the value of the variable name
+    Pay Rate: $ //output the value of the variable rate
+    Hours Worked: //output the value of the variable //hoursWorked
+    Salary: $ //output the value of the variable wages
+
+For example, if the value of name is "Rainbow" and hoursWorked is
+45.50, then the output is:
+
+    Name: Rainbow
+    Pay Rate: $12.50
+    Hours Worked: 45.50
+    Salary: $568.75
+
+m. Write a C++ program that tests each of the C++ statements that you
+wrote in parts a through l. Place the statements at the appropriate place
+in the previous C++ program segment. Test run your program (twice)
+on the following input data:
+
+    a.  num1 = 13, num2 = 28; name = "Jacobson"; hoursWorked =
+    48.30.
+    b.  num1 = 32, num2 = 15; name = "Crawford"; hoursWorked =
+    58.45. */
+
 #include <iostream>
 #include <string>
 #include <iomanip>
