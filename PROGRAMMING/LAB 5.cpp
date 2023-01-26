e01afb9c6379b12c05ec5effb69b9f821de527df
ewwina <60821798+cherrycho@users.noreply.github.com>
2023-01-27 06:59:29 +0800

    Update LAB 5.cpp

--- PROGRAMMING/LAB 5.cpp
+++ PROGRAMMING/LAB 5.cpp
@@ -1,10 +1,12 @@
-/* The following program is designed to find the area of a rectangle, the area of
+/* 
+The following program is designed to find the area of a rectangle, the area of
 a circle, or the volume of a cylinder. However, (a) the statements are in the
 incorrect order; (b) the function calls are incorrect; (c) the logical expression
 in the while loop is incorrect; and (d) function definitions are incorrect.
 Rewrite the program so that it works correctly. Your program must be
 properly indented. (Note that the program is menu driven and allows the
-user to run the program as long as the user wishes.)*/
+user to run the program as long as the user wishes.)
+*/
 
 #include <iostream>
 #include <iomanip>
